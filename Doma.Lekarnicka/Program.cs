988c4d1eaab245d25270f87ca8718f32141b8dc2
Veronika Šalplachtová <veronika@salplachta.net>
2022-10-15 12:37:47 +0200

    Add project files.

---
+++ Doma.Lekarnicka/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Doma.Lekarnicka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
