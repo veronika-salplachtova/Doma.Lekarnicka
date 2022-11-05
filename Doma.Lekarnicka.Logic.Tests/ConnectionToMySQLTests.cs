4028788d5f580c617cf9b3afa47824d31d77be6e
Veronika Šalplachtová <veronika@salplachta.net>
2022-11-05 18:50:43 +0100

    Pridano pripojeni k databazi

---
+++ Doma.Lekarnicka.Logic.Tests/ConnectionToMySQLTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Doma.Lekarnicka.Logic.Tests;
+
+public class ConnectionToMySQLTests
+{
+    [Fact]
+    public void ConnectToDbIsSuccessfull()
+    {
+        ConnectionToMySQL connection = new();
+        Assert.True(connection.Connect());
+    }
+}
---
+++ Doma.Lekarnicka.Logic/ConnectionToMySQL.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Doma.Lekarnicka.Logic
+{
+    public class ConnectionToMySQL
+    {
+        MySqlConnection conn;
+
+
+        public bool Connect()
+        {
+            string myConnectionString = "server=lekarnicka.cizzqrz9ailg.us-east-1.rds.amazonaws.com;uid=LekarnickaUser;pwd=1234;database=Lekarnicka";
+
+            try
+            {
+                conn = new MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                return true;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                // MessageBox.Show(ex.Message);
+            }
+            return false;
+        }   
+    }
+}
