4015129ab443aa60bad9ce19a00be43c075dd6a8
Veronika Šalplachtová <veronika@salplachta.net>
2022-11-06 17:18:04 +0100

    uprava zapisu

--- Doma.Lekarnicka.Logic/ConnectionToMySQL.cs
+++ Doma.Lekarnicka.Logic/ConnectionToMySQL.cs
@@ -6,14 +6,13 @@ namespace Doma.Lekarnicka.Logic
     {
         MySqlConnection conn;
 
-
         public bool Connect()
         {
             string myConnectionString = "server=lekarnicka.cizzqrz9ailg.us-east-1.rds.amazonaws.com;uid=LekarnickaUser;pwd=1234;database=Lekarnicka";
 
             try
             {
-                conn = new MySqlConnection();
+                conn = new ();
                 conn.ConnectionString = myConnectionString;
                 conn.Open();
                 return true;
