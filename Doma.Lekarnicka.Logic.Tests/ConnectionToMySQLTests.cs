4b9928770feb25e7fdc52d8d3bb0b1b1f5a189b6
Veronika Šalplachtová <veronika@salplachta.net>
2022-11-07 08:56:41 +0100

    Doplneni metod ExecuteReader a ExecuteNonQuery

--- Doma.Lekarnicka.Logic.Tests/ConnectionToMySQLTests.cs
+++ Doma.Lekarnicka.Logic.Tests/ConnectionToMySQLTests.cs
@@ -1,4 +1,6 @@
-﻿namespace Doma.Lekarnicka.Logic.Tests;
+﻿using MySql.Data.MySqlClient;
+
+namespace Doma.Lekarnicka.Logic.Tests;
 
 public class ConnectionToMySQLTests
 {
@@ -7,5 +9,28 @@ public class ConnectionToMySQLTests
     {
         ConnectionToMySQL connection = new();
         Assert.True(connection.Connect());
+    }
+
+    [Fact]
+    public void ExecuteReaderWithSelect_ReturnsData()
+    {
+        ConnectionToMySQL connection = new();
+        connection.Connect();
+        string sql = "SELECT 1";
+        MySqlDataReader reader = connection.ExecuteReader(sql);
+        reader.Read();
+        int result = reader.GetInt32(0);   
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+ 
+    public void ExecuteNonReader_ReturnsCountOfRemovedItem()
+    {
+        ConnectionToMySQL connection = new();
+        connection.Connect();
+        string sql = "DELETE FROM Drugs WHERE 1 = 0";
+        int result = connection.ExecuteNonQuery(sql);
+        Assert.Equal(0, result);
     }
 }
--- Doma.Lekarnicka.Logic/ConnectionToMySQL.cs
+++ Doma.Lekarnicka.Logic/ConnectionToMySQL.cs
@@ -1,27 +1,51 @@
 ﻿using MySql.Data.MySqlClient;
 
-namespace Doma.Lekarnicka.Logic
+namespace Doma.Lekarnicka.Logic;
+
+public class ConnectionToMySQL
 {
-    public class ConnectionToMySQL
+    MySqlConnection conn;
+    string myConnectionString = "server=lekarnicka.cizzqrz9ailg.us-east-1.rds.amazonaws.com;uid=LekarnickaUser;pwd=1234;database=Lekarnicka";
+
+    public bool Connect()
     {
-        MySqlConnection conn;
-
-        public bool Connect()
+        try
         {
-            string myConnectionString = "server=lekarnicka.cizzqrz9ailg.us-east-1.rds.amazonaws.com;uid=LekarnickaUser;pwd=1234;database=Lekarnicka";
-
-            try
-            {
-                conn = new ();
-                conn.ConnectionString = myConnectionString;
-                conn.Open();
-                return true;
-            }
-            catch (MySql.Data.MySqlClient.MySqlException ex)
-            {
-                // MessageBox.Show(ex.Message);
-            }
+            conn = new();
+            conn.ConnectionString = myConnectionString;
+            conn.Open();
+            return true;
+        }
+        catch
+        {
             return false;
-        }   
+        }
+    }
+
+    public MySqlDataReader ExecuteReader(string sql)
+    {
+        try
+        {
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public int ExecuteNonQuery(string sql)
+    {
+        try
+        {
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            return cmd.ExecuteNonQuery();
+        }
+        catch
+        {
+            return -1;
+        }
     }
 }
